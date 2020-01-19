02ff2a657e1a4f8349dc689e726abafb82780be9
Jim Strother <jim.strother@gmail.com>
2020-01-19 13:02:50 -0800

    Debug: getting [object Object] on axios
    data coming from 3rd-party API, but not in readable form, just type is coming through

--- api/title.js
+++ api/title.js
@@ -3,20 +3,41 @@ const express = require('express');
 const axios = require('axios');
 
 const app = express();
+const jsonParser = express.json();
+
+app.use(jsonParser);
 
 // GET - /api/title
 app.get('/', (req, res) => {
-  const recommendationsURL = `https://tastedive.com/api/similar?q=and+then+there+were+none&type=books&info=1&k=${process.env.TASTE_DIVE_API_KEY}`;
+  // consola.ready({
+  //   message: `req.body ${req.json()}`,
+  //   badge: true,
+  // });
+  const title = 'and then there were none'; // this is just until data is coming from user input
+
+  const replaceSpaces = (str) => str.split(' ').join('+');
 
   axios
-    .get(recommendationsURL)
+    .get('https://tastedive.com/api/similar', {
+      params: {
+        q: replaceSpaces(title),
+        type: 'books',
+        info: 1,
+        limit: 7,
+        k: process.env.TASTE_DIVE_KEY,
+      },
+    })
     .then(({ data }) => {
+      consola.ready({
+        message: `data from axios server ${data}`,
+        badge: true,
+      });
       res.json(data);
     })
     .catch((error) => {
       res.status(400).json(error);
       consola.error({
-        message: `error from axios server ${error} `,
+        message: `error from axios server ${error}`,
         badge: true,
       });
     });
