c64e4dd8a712a86a9abcefb22f64360373a76f3b
Jim Strother <jim.strother@gmail.com>
2020-01-19 15:53:14 -0800

    Debug: api/title.js does not return data
    postman will return data in full, whereas api/title.js will not return anything in Results from tastedive

--- api/title.js
+++ api/title.js
@@ -9,10 +9,15 @@ app.use(jsonParser);
 
 // GET - /api/title
 app.get('/', (req, res) => {
-  // consola.ready({
-  //   message: `req.body ${req.json()}`,
-  //   badge: true,
-  // });
+  // req.json().then((data) => console.log(`req: ${data}`)); // throws error
+  // res.json().then((data) => console.log(`res: ${data}`)); // throws error
+
+  // console.log(`req: ${JSON.stringify(req)}`); // throws error
+  // console.log(`res: ${JSON.stringify(res)}`); // throws error
+
+  // console.log(`req: ${req}`); // req: [object Object]
+  // console.log(`res: ${res}`); // res: [object Object]
+
   const title = 'and then there were none'; // this is just until data is coming from user input
 
   const replaceSpaces = (str) => str.split(' ').join('+');
