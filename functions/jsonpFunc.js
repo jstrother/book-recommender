acd9f15073d20b37d0c11709a176eda2798da31f
Jim Strother <jim.strother@gmail.com>
2020-02-26 20:36:53 -0800

    Moved: jsonpFunc.test.js from test/api/ to test/functions/

--- functions/jsonpFunc.js
+++ functions/jsonpFunc.js
@@ -1,21 +1,28 @@
 const jsonpFunc = (function() {
   const dataObject = {};
 
-  dataObject.send = (src, options) => {
-    const callbackName = options.callbackName || 'callback';
-    const onSuccess = options.onSuccess || function() {};
-    const onTimeout = options.onTimeout || function() {};
-    const timeout = options.timeout || 10; // seconds
+  dataObject.send = (src, options = {}) => {
+    const callbackName = options.callbackName ? options.callbackName : 'callback';
+    const onSuccess = options.onSuccess ? options.onSuccess : function() {};
+    const onTimeout = options.onTimeout ? options.onTimeout : function() {};
+    const timeout = options.timeout ? options.timeout : 10; // seconds
 
-    const timeoutTrigger = setTimeout(function() {
-      process[callbackName] = function() {};
+    const timeoutTrigger = window.setTimeout(function() {
+      window[callbackName] = function() {};
       onTimeout();
     }, timeout * 1000);
 
-    process[callbackName] = function(data) {
-      process.clearTimeout(timeoutTrigger);
+    window[callbackName] = function(data) {
+      window.clearTimeout(timeoutTrigger);
       onSuccess(data);
     };
+
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.async = true;
+    script.src = src;
+
+    document.head.appendChild(script);
   };
 
   return dataObject;
